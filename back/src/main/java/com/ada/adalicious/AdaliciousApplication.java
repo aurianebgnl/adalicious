c01ddac8973b833eb557d15aa5ae96f9456509f2
aurianebgnl <auriane.bgnl@gmail.com>
2025-07-11 09:51:08 +0200

    first commit

---
+++ back/src/main/java/com/ada/adalicious/AdaliciousApplication.java
@@ -0,0 +1,13 @@
+package com.ada.adalicious;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaliciousApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaliciousApplication.class, args);
+	}
+
+}
